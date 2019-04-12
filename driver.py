26751f3379a6624085cb2a419987b43b6c8828d2
Julian Sweatt <20158985+juliansweatt@users.noreply.github.com>
2019-04-12 14:15:52 -0400

    Seperated Driver & Stock Object Implementations

---
+++ driver.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+def __int_in_range__(i):
+    """Argument filter for an integer between 0 and 110.
+
+    :param str i: The number to filter.
+    :return: The given number.
+    :rtype: int
+    :raises ArgumentTypeError: If the number is not an integer between 0 and 110.
+    """
+    inti = int(i)
+    if inti < 0 or inti > 110:
+        raise argparse.ArgumentTypeError("0 < ticker_count <= 110 is not satisfied by %d" % i)
+    return inti
+
+def parseInput():
+    """Parse arguments from standard input.
+
+    :return: Parsed arguments.
+    :rtype: argparse.Namespace
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--operation", type=str, choices=("Ticker","Fetcher","Query"), help="Operation to perform.", required=True)
+    parser.add_argument("--ticker_count", type=__int_in_range__, help="Number of Tickers to Retrieve (0 < ticker_count <= 110)", required=(parser.parse_known_args()[0].operation in ("Ticker","Fetcher")))
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    # Parse Input
+    args = parseInput()
+
+    # Execute Cooresponding Operation
+    if args.operation == "Ticker":
+        Tickers().save_tickers(args.ticker_count)
+    elif args.operation == "Fetcher":
+        Fetcher().fetch(60) #bookmark testing as class wihout DB
